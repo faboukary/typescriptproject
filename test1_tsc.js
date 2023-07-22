208ca3906ecaa107eed657be2bdf3e905d33c31b
faboukary <koulibaly.faboukary@gmail.com>
2023-07-22 11:27:41 +0000

    mise à jour du script test1_tsc.ts

---
+++ test1_tsc.js
@@ -0,0 +1,7 @@
+var fruits = ["Apples", "Mangoes", "Bananas"];
+var vegetables = ["Potatoes", "Onions", "Cucumbers"];
+fruits.push("Orange");
+// ["Apples", "Mangoes", "Bananas", "Orange"] 
+fruits[0] = "Papayas";
+// ["Papayas", "Mangoes", "Bananas", "Orange"] 
+// fruits = vegetables // Error
---
+++ test1_tsc.ts
@@ -0,0 +1,7 @@
+const fruits = ["Apples", "Mangoes", "Bananas"];
+const vegetables = ["Potatoes", "Onions", "Cucumbers"];
+fruits.push("Orange");
+// ["Apples", "Mangoes", "Bananas", "Orange"] 
+fruits[0] = "Papayas";
+// ["Papayas", "Mangoes", "Bananas", "Orange"] 
+fruits = vegetables // Error
